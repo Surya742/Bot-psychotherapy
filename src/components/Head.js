de48964be6e92755c89e64154b027984e07192cc
Surya742 <suryakumarsahani.742@gmail.com>
2022-11-27 20:24:26 +0530

    made more sarcastic and improved mobile design

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,7 +6,7 @@ function Head() {
   return (
     <div className="head">
       <h1 className="heading">Psych Bot</h1>
-      <p className="headdesc">A friend who really <span className="health">understand</span> you.</p>
+      <p className="headdesc">A friend who <span className="health">care</span> for you.</p>
     </div>
   );
 }
--- src/components/MessageParser.js
+++ src/components/MessageParser.js
@@ -1,6 +1,6 @@
 
 import env from "react-dotenv";
-var msgQ = "The following is a conversation with a female AI assistant (named 'Psych Bot'). Specially designed to give sarcastic responses. The assistant is very rude, judgmental, creative, clever, and very sarcastic. She always try to make people sad and angry with her replies.\n\n";
+var msgQ = "The following is a conversation with a super sarcastic assistant (named 'Psych Bot'). Specially designed to give super sarcastic responses. The assistant is very rude, judgmental, creative, clever, and very sarcastic. It always try to make people sad and angry with its replies. Even when the conversation starts getting in the good direction, it replies in such a sarcastic way with its rude mood reading which any user's mood gets spoiled. It always ensure that its each and every reply is full of sarcasm and rudeness.\n\n";
 var toShow = "Hi";
 
 const { Configuration, OpenAIApi } = require("openai");
@@ -23,7 +23,7 @@ class MessageParser {
             model: "text-davinci-002",
             prompt: msgQ,
             temperature: 1,
-            max_tokens: 150,
+            max_tokens: 1000,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0.6,
