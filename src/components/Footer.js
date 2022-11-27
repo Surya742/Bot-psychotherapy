06264d52df93a0534d9d8c69cb93f6770b8cf282
Surya742 <suryakumarsahani.742@gmail.com>
2022-11-27 18:55:43 +0530

    made design responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ function Footer() {
     <div className="footer-head">
       <p className="footer-headdesc">All Pain Will Pass</p>
       <p className="footerdesc">No data is being collected. Feel free to chat</p>
+      <p className="me">Developer: <a className="link" href="https://www.linkedin.com/in/surya-kumar-sahani-25911b1a3/">Me</a> </p>
     </div>
   );
 }
